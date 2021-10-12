057de4da4649df7346d71c64b5c5d23fa7f1b730
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-10-12 19:50:38 -0400

    Added Internet Gateway in infra

--- main.tf
+++ main.tf
@@ -1,10 +1,35 @@
 resource "aws_vpc" "vpc_a2" {
-  cidr_block                       = "10.0.0.0/16"
+  cidr_block                       = var.vpc_cidr_block
   enable_dns_hostnames             = true
   enable_dns_support               = true
   enable_classiclink_dns_support   = true
-  assign_generated_ipv6_cidr_block = true
+  assign_generated_ipv6_cidr_block = false
+
   tags = {
     Name = "vpc_a2"
+  }
+}
+
+resource "aws_subnet" "subnet_a2" {
+
+  // Specifying an explicit dependency of this subnet on vpc 
+  depends_on = [aws_vpc.vpc_a2]
+
+  vpc_id = aws_vpc.vpc_a2.id
+
+  for_each          = var.subnet_az_cidr
+  availability_zone = each.key
+  cidr_block        = each.value
+
+  tags = {
+    Name = "subnet_a2"
+  }
+}
+
+resource "aws_internet_gateway" "igw_a2" {
+  vpc_id = aws_vpc.vpc_a2.id
+
+  tags = {
+    Name = "igw_a2"
   }
 }
