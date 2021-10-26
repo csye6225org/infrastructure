53688be8f465416748ed5318f2f96d9dbd151e84
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-10-26 10:44:18 -0400

    Corrected Ec2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-
 // Virtual Private Cloud
+
 resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr_block
+  cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
 
   tags = {
@@ -11,30 +11,42 @@ resource "aws_vpc" "vpc" {
 
 // Subnet
 // There are 3 subnets, each in a seperate availability zone
-// The depends on specifies an explicit dependency of these subnets on vpc
-resource "aws_subnet" "subnet" {
 
-  depends_on = [aws_vpc.vpc]
-
-  vpc_id = aws_vpc.vpc.id
-
-  for_each          = var.subnet_az_cidr
-  availability_zone = each.key
-  cidr_block        = each.value
-
+resource "aws_subnet" "subnet1" {
+  depends_on        = [aws_vpc.vpc]
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = keys(var.subnet_az_cidr_map)[0]
+  cidr_block        = values(var.subnet_az_cidr_map)[0]
   tags = {
-    Name = "subnet"
+    Name = "subnet1"
+  }
+}
+resource "aws_subnet" "subnet2" {
+  depends_on        = [aws_vpc.vpc]
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = keys(var.subnet_az_cidr_map)[1]
+  cidr_block        = values(var.subnet_az_cidr_map)[1]
+  tags = {
+    Name = "subnet2"
+  }
+}
+resource "aws_subnet" "subnet3" {
+  depends_on        = [aws_vpc.vpc]
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = keys(var.subnet_az_cidr_map)[2]
+  cidr_block        = values(var.subnet_az_cidr_map)[2]
+  tags = {
+    Name = "subne3"
   }
 }
 
+
 // Internet Gateway
 // For the Vpc
+
 resource "aws_internet_gateway" "igw" {
-
   depends_on = [aws_vpc.vpc]
-
-  vpc_id = aws_vpc.vpc.id
-
+  vpc_id     = aws_vpc.vpc.id
   tags = {
     Name = "igw"
   }
@@ -42,19 +54,14 @@ resource "aws_internet_gateway" "igw" {
 
 // Route table
 // This is a custom route table
-// Has  route in between every IPv4 addresses and 
-// Internet gateway
+
 resource "aws_route_table" "public_rt" {
-
   depends_on = [aws_vpc.vpc, aws_internet_gateway.igw]
-
-  vpc_id = aws_vpc.vpc.id
-
+  vpc_id     = aws_vpc.vpc.id
   route {
     cidr_block = var.rt_destination_cidr_block
     gateway_id = aws_internet_gateway.igw.id
   }
-
   tags = {
     Name = "public_rt"
   }
@@ -62,16 +69,167 @@ resource "aws_route_table" "public_rt" {
 
 // Route table association 
 // Shows association in between subnets and public route table
-// Destination: Subnet
-// Target: Route table
-resource "aws_route_table_association" "rta" {
 
-  for_each = var.subnet_az_cidr
-
-  subnet_id      = aws_subnet.subnet[each.key].id
+resource "aws_route_table_association" "rta1" {
+  depends_on     = [aws_subnet.subnet1, aws_route_table.public_rt]
+  subnet_id      = aws_subnet.subnet1.id
   route_table_id = aws_route_table.public_rt.id
-
-  depends_on = [aws_subnet.subnet, aws_route_table.public_rt]
-
+}
+resource "aws_route_table_association" "rta2" {
+  depends_on     = [aws_subnet.subnet2, aws_route_table.public_rt]
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_route_table_association" "rta3" {
+  depends_on     = [aws_subnet.subnet3, aws_route_table.public_rt]
+  subnet_id      = aws_subnet.subnet3.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
+// Application Security Group
+
+resource "aws_security_group" "application_sg" {
+  name   = "application_sg"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.source_cidr_block]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.source_cidr_block]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.source_cidr_block]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.source_cidr_block]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "application_sg"
+  }
+}
+
+// Database Security Group
+
+resource "aws_security_group" "database_sg" {
+  name   = "database_sg"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.application_sg.id]
+  }
+  tags = {
+    Name = "database_sg"
+  }
+}
+
+// S3 Bucket
+
+resource "random_string" "randomstring" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = format("%s.%s.%s", random_string.randomstring.result, var.enviornment, var.domain_name)
+  lifecycle_rule {
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+// Relational Database
+
+resource "aws_db_parameter_group" "db_pg" {
+  family = var.rds_family
+}
+
+resource "aws_db_subnet_group" "db_sg" {
+  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id, aws_subnet.subnet3.id]
+  tags = {
+    Name = "db_subnet_group"
+  }
+}
+
+resource "aws_db_instance" "rds" {
+  allocated_storage    = var.rds_allocated_storage
+  engine               = var.rds_engine
+  engine_version       = var.rds_engine_version
+  instance_class       = var.rds_db_instance_class
+  multi_az             = var.rds_multi_az_allowance
+  identifier           = var.rds_db_identifier
+  username             = var.rds_db_username
+  password             = var.rds_db_password
+  db_subnet_group_name = aws_db_subnet_group.db_sg.id
+  parameter_group_name = aws_db_parameter_group.db_pg.id
+  publicly_accessible  = var.rds_db_public_accessibility
+  name                 = var.rds_db_name
+  skip_final_snapshot  = var.rds_db_skip_final_snapshot
+}
+
+// EC2 Instance
+
+resource "aws_instance" "ec2" {
+  ami                     = var.ec2_source_ami
+  instance_type           = var.ec2_instance_type
+  security_groups         = [aws_security_group.application_sg.id]
+  depends_on              = [aws_db_instance.rds]
+  subnet_id               = aws_subnet.subnet1.id
+  // disable_api_termination = var.ec2_disable_api_termination_flag
+  associate_public_ip_address = var.ec2_public_ipv4_association_flag
+  key_name = var.ec2_ssh_key_name
+  ebs_block_device {
+    device_name           = var.ec2_device_name
+    delete_on_termination = var.ec2_delete_on_termination_flag
+    volume_type           = var.ec2_volume_type
+    volume_size           = var.ec2_volume_size
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              export DB_HOST=${aws_db_instance.rds.address}
+              export DB_PORT=${aws_db_instance.rds.port}
+              export DB_DATABASE=${var.ec2_env_db_name}
+              export DB_USERNAME=${var.ec2_env_db_username}
+              export DB_PASSWORD=${var.ec2_env_db_password}
+              # export FILESYSTEM_DRIVER=s3
+              export S3_BUCKET_NAME=${aws_s3_bucket.s3_bucket.bucket}
+              export S3_BUCKET_ID=${aws_s3_bucket.s3_bucket.id}
+              export AWS_DEFAULT_REGION=${var.ec2_env_aws_region}
+              export AWS_ACCESS_KEY=${var.ec2_env_aws_access_key}
+              export AWS_SECRET_ACCESS_KEY=${var.ec2_env_aws_secret_access_key}
+              EOF
+
+  tags = {
+    "Name" = "ec2"
+  }
+} 
--- variables.tf
+++ variables.tf
@@ -1,24 +1,152 @@
-// 1.
-// This will take String of
-// CIDR block for the VPC
+// ############################################
+//  Network Variables
+// ############################################
+
 variable "vpc_cidr_block" {
   type        = string
-  description = "CIDR for VPC"
+  description = "CIDR block for VPC"
 }
-
-// 2.
-// This will take Map of 
-// Availability Zones as Key
-// CIDR block for Subnet as Value
-variable "subnet_az_cidr" {
+variable "subnet_az_cidr_map" {
   type        = map(string)
   description = "Availability zone to CIDR map for subnet"
 }
-
-// 3. 
-// This will take CIDR Block 
-// For destination in route in route table 
 variable "rt_destination_cidr_block" {
   type        = string
   description = "Destination CIDR block for Route Table"
 }
+variable "source_cidr_block"{
+  type = string
+  description = "Source CIDR block for Application security group"
+}
+variable "enviornment" {
+  type        = string
+  description = "Enviornment in which we are working, dev or prod"
+}
+variable "domain_name" {
+  type        = string
+  description = "My domain name, eg. example.com"
+}
+
+// ############################################
+//  Database Variables
+// ############################################
+
+variable "rds_family"{
+  type = string
+  description = "Database parameter group family"
+}
+variable "rds_allocated_storage" {
+  type        = number
+  description = "Allocated storage for database"
+}
+variable "rds_engine" {
+  type        = string
+  description = "Database engine"
+}
+variable "rds_engine_version" {
+  type        = number
+  description = "Database Engine version"
+}
+variable "rds_db_instance_class" {
+  type        = string
+  description = "Database Instance Class"
+}
+variable "rds_multi_az_allowance" {
+  type        = bool
+  description = "Allowance for multi az deployment"
+}
+variable "rds_db_identifier" {
+  type        = string
+  description = "Database Identifier"
+}
+variable "rds_db_username" {
+  type        = string
+  description = "Database Username"
+}
+variable "rds_db_password" {
+  type        = string
+  description = "Database password"
+}
+variable "rds_db_public_accessibility" {
+  type        = bool
+  description = "Allowance for database to be publicly accessible"
+}
+variable "rds_db_name" {
+  type        = string
+  description = "Database name"
+}
+variable "rds_db_skip_final_snapshot" {
+  type        = bool
+  description = "Allowance for skipping final snapshot creation"
+}
+
+// ############################################
+//  Ec2 Instance Variables
+// ############################################
+
+variable "ec2_source_ami" {
+  type        = string
+  description = "Source Ami for EC2"
+}
+variable "ec2_instance_type" {
+  type        = string
+  description = "EC2 Instance type"
+}
+variable "ec2_disable_api_termination_flag"{
+  type        = bool
+  description = "Disable api termination flag"
+}
+variable "ec2_public_ipv4_association_flag"{
+  type = bool
+  description = "Associate public Ipv4 flag"
+}
+variable "ec2_ssh_key_name"{
+  type = string
+  description = "SSH key name to connect with Ec2 instance"
+}
+variable "ec2_device_name" {
+  type        = string
+  description = "EC2 Device name"
+}
+variable "ec2_delete_on_termination_flag"{
+  type        = bool
+  description = "EBS block delete on termination flag"
+}
+variable "ec2_volume_type"{
+  type        = string
+  description = "EBS block volume type"
+}
+variable "ec2_volume_size"{
+  type        = number
+  description = "EBS block volume size"
+}
+
+// ############################################
+//  Ec2 Instance Enviornment Variables
+// ############################################
+
+variable "ec2_env_db_name"{
+  type = string
+  description = "Database name in AWS RDS"
+}
+variable "ec2_env_db_username"{
+  type = string
+  description = "Database username in AWS RDS"
+}
+variable "ec2_env_db_password"{
+  type = string
+  description = "Database password in AWS RDS"
+}
+variable "ec2_env_aws_region"{
+  type = string
+  description = "AWS Region"
+}
+variable "ec2_env_aws_access_key"{
+  type = string
+  description = "AWS access key"
+}
+variable "ec2_env_aws_secret_access_key"{
+  type = string
+  description = "AWS secret access key"
+}
+
